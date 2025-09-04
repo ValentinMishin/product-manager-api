f21290914f80e4d819081daf6a211d9eb111b8da
Valentin <mvalentin.dev@gmail.com>
2025-09-04 18:41:45 +0700

    Инициализация

---
+++ src/main/java/ru/valentin/product_manager_api/ProductManagerApiApplication.java
@@ -0,0 +1,13 @@
+package ru.valentin.product_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/valentin/product_manager_api/ProductManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.valentin.product_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
