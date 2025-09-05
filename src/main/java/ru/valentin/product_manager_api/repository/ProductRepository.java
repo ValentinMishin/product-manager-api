5ee8ff00e71636f365bcf4202b3803e6556d1d1f
Valentin <mvalentin.dev@gmail.com>
2025-09-05 22:35:16 +0700

    Добавлены поиск с фильтром по цене с пагинацией

--- src/main/java/ru/valentin/product_manager_api/repository/ProductRepository.java
+++ src/main/java/ru/valentin/product_manager_api/repository/ProductRepository.java
@@ -1,11 +1,16 @@
 package ru.valentin.product_manager_api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.valentin.product_manager_api.model.Product;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByExternalId(Long externalId);
     boolean existsByExternalId(Long externalId);
+
+    Page<Product> findByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice, Pageable pageable);
 }
