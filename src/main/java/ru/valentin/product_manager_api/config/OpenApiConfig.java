2355c3d315c7645bff0fe3b2b1a2dfa41bcaeadb
Valentin <mvalentin.dev@gmail.com>
2025-09-05 16:40:00 +0700

    Создан конфиг для swagger

---
+++ src/main/java/ru/valentin/product_manager_api/config/OpenApiConfig.java
@@ -0,0 +1,16 @@
+package ru.valentin.product_manager_api.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Product API",
+                version = "1.0",
+                description = "API для управления товарами с синхронизацией из внешнего сервиса"
+        )
+)
+public class OpenApiConfig {
+}
