fbfb7c02f2ebf6bc3052d318abe3cdd34c2cc25d
Valentin <mvalentin.dev@gmail.com>
2025-09-07 18:26:45 +0700

    Создана сущность  пользователь для разграничения прав доступа к апи

---
+++ src/main/java/ru/valentin/product_manager_api/model/User.java
@@ -0,0 +1,137 @@
+package ru.valentin.product_manager_api.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true)
+    private String clientName;
+
+    @Column(name = "password")
+    private String clientPassword;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Set<UserRole> roles;
+
+    public User() {
+        this.roles = Set.of(UserRole.ROLE_USER);
+    }
+
+    public User(String clientName, String clientPassword) {
+        this.clientName = clientName;
+        this.clientPassword = clientPassword;
+        this.roles = Set.of(UserRole.ROLE_USER);
+    }
+
+    public User(String clientName, String clientPassword, Set<UserRole> roles) {
+        this.clientName = clientName;
+        this.clientPassword = clientPassword;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientPassword() {
+        return clientPassword;
+    }
+
+    public void setClientPassword(String clientPassword) {
+        this.clientPassword = clientPassword;
+    }
+
+    public Set<UserRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<UserRole> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getPassword() {
+        return clientPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return clientName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void addRole(UserRole role) {
+        this.roles.add(role);
+    }
+
+    public void removeRole(UserRole role) {
+        this.roles.remove(role);
+    }
+
+    public boolean hasRole(UserRole role) {
+        return this.roles.contains(role);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", clientName='" + clientName + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
+}
