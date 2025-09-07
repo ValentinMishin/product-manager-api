df8527c77dd8cd4fe35a2703257e9dd539749884
Valentin <mvalentin.dev@gmail.com>
2025-09-07 16:01:41 +0700

    Добавлены метода поиска продуктов по айди категории и названию

--- src/main/java/ru/valentin/product_manager_api/repository/ProductRepository.java
+++ src/main/java/ru/valentin/product_manager_api/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package ru.valentin.product_manager_api.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.valentin.product_manager_api.model.Product;
 
 import java.math.BigDecimal;
@@ -13,4 +15,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsByExternalId(Long externalId);
 
     Page<Product> findByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice, Pageable pageable);
+
+    @Query("SELECT p FROM Product p WHERE p.category.id = :categoryId")
+    Page<Product> findByCategoryId(@Param("categoryId") Long categoryId, Pageable pageable);
+
+    @Query("SELECT p FROM Product p WHERE LOWER(p.category.title) = LOWER(:categoryTitle)")
+    Page<Product> findByCategoryTitle(@Param("categoryTitle") String categoryTitle, Pageable pageable);
 }
