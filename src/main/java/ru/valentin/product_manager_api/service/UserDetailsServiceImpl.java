8846c0a9b6014cc7ccbc2d6a39eae2051d6a283b
Valentin <mvalentin.dev@gmail.com>
2025-09-07 18:28:27 +0700

    Создана имплементация UserDetails для работы spring security

---
+++ src/main/java/ru/valentin/product_manager_api/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package ru.valentin.product_manager_api.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.valentin.product_manager_api.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByClientName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Пользователь с логином" + username + "не найден"));
+
+    }
+}
