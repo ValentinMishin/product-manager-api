bfcec3a55dfc2fa1865ad8157b0cf3d752b185cd
Valentin <mvalentin.dev@gmail.com>
2025-09-08 09:22:15 +0700

    Добавлено начальное заполнение пользователями с возможными ролями

---
+++ db/init/02-data.sql
@@ -0,0 +1,11 @@
+TRUNCATE TABLE product, category, rating, users, user_role  RESTART IDENTITY;
+
+-- admin : admin
+-- client : client
+insert into user_role(user_id, role) values
+(1, 'ROLE_ADMIN'),
+(2, 'ROLE_USER');
+
+insert into users (username, password) values
+('admin', '$2y$12$l9nyPaWm917Nt2zxWMU6J.ThdCnQ/RdbGlYotMeg1zc9MNPDHFfAe'),
+('client', '$2y$12$fVL.tbFwWIuwde6SAsjgOuNaxFtvff9RGEoCY4ZhANJ1om0.SIq2O');
