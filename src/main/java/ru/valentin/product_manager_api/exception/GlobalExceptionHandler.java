2dd145f2c3eeec1241f236ecb45f03fb2156c3ab
Valentin <mvalentin.dev@gmail.com>
2025-09-08 09:17:52 +0700

    Добавлен обработчик исключения не найденной сущности в БД

--- src/main/java/ru/valentin/product_manager_api/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/valentin/product_manager_api/exception/GlobalExceptionHandler.java
@@ -1,12 +1,14 @@
 package ru.valentin.product_manager_api.exception;
 
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.valentin.product_manager_api.dto.BaseExceptionResponseDto;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -24,5 +26,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException e) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Доступ запрещен");
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<BaseExceptionResponseDto> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                new BaseExceptionResponseDto(
+                        HttpStatus.NOT_FOUND.toString(),
+                        e.getMessage()
+                )
+        );
     }
 }
